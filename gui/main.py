6d502121d7924a0a2270cab0166117a5123ab1fa
badouuuuuu <badounet01@gmail.com>
2020-11-30 21:24:27 +0100

    try to import my_sqlite3_v1 and create kv function for top menu button

--- gui/main.py
+++ gui/main.py
@@ -9,13 +9,25 @@ from kivy.uix.button import Button
 from kivy.core.window import Window
 from kivy.uix.widget import Widget
 
+from __init__ import *
+
 Window.size = (1024, 768)
 Window.minimum_width, Window.minimum_height = Window.size
 
 
 class MyGridLayout(Widget):
-        print('Labo 1 - App General ')
+        def add_user(self):
+            print('Add User')
         # Mise en place de class, mise au propre du fichier gui.py
+        
+        def modify_user(self):
+            print('Modify User')
+            
+        def add_menu(self):
+            print('Add menu')
+        
+        def modify_menu(self):
+            print('Modify menu')
 
 class Meals(App):
     def build(self):
