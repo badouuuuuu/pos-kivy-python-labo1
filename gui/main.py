d87dad073b88ee73b8ea01a80671386ae101b68f
badouuuuuu <badounet01@gmail.com>
2020-12-01 11:44:49 +0100

    fix popup color and atlas background

--- gui/main.py
+++ gui/main.py
@@ -28,7 +28,15 @@ class PopUpShow(FloatLayout):
     def show_popup():
         show = PopUpShow() # Create a new instance of the P class 
 
-        popupWindow = Popup(title="Identifiant introuvable", content=show, size_hint=(None,None),size=(600,400)) 
+        popupWindow = Popup(
+            title="Identifiant introuvable", 
+            content=show, 
+            size_hint=(None,None),
+            size=(600,400),
+            background = 'atlas://data/images/defaulttheme/button_pressed',
+            background_color = (0,0,0.1,0.75),
+            opacity = 1
+                            ) 
         # Create the popup window
 
         popupWindow.open() # show the popup
